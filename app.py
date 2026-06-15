64376a95080338b16f73699a891b9845b66d81e7
Saubhagya Tiwari <saubhagyatiwari19@gmail.com>
2026-06-15 16:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+from supabase import create_client
+
+st.set_page_config(page_title="Finance Tracker")
+
+st.title("💰 Smart Finance Tracker")
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+try:
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+    st.success("✅ Supabase Connected Successfully!")
+
+except Exception as e:
+    st.error(f"Connection Failed: {e}")
